6d842307f6cb42e736e416d2819b706f9e2ac296
Abdelhamid El kemmal <abdel-ke@e2r1p5.1337.ma>
2021-02-25 15:19:53 +0100

    2021/02/25_modif_nginx

--- nginx/rm_docker.sh
+++ nginx/rm_docker.sh
@@ -4,10 +4,5 @@ docker images
 if (($1  == 1))
 then
 	echo "\n\n"
-	docker rm $2
-	docker image rm -f $3 $4
-	echo "\n\n"
-	docker ps -a
-	echo "\n"
-	docker images
+	docker rm -f $(docker ps -a -q)
 fi
